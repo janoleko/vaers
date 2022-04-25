ff550af3ba4c0ff5d4cd498ae7ab85047c1495b2
janoleko <janole.koslik@gmail.com>
2022-04-25 13:48:01 +0200

    Corrections

--- R/create_dataset.R
+++ R/create_dataset.R
@@ -6,15 +6,15 @@
 #' @return Returns the filtered dataset with a new column order.
 #' @export
 #'
-#' @examples create_dataset(vaers_2021, "COVID19 (COVID19 (JANSSEN))")
-#' @examples create_dataset(.data = vaers_2021, vaxname = "COVID19 (COVID19 (JANSSEN))")
+#' @examples create_dataset(vaers_2020, "COVID19 (COVID19 (JANSSEN))")
+#' @examples create_dataset(.data = vaers_2020, vaxname = "COVID19 (COVID19 (JANSSEN))")
 #' @examples vaers_2020 %>% create_dataset(vaxname = "COVID19 (COVID19 (JANSSEN))")
 #' @examples vaers_2018 %>% create_dummy("headache", c("Headache")) %>% create_dataset("ZOSTER (SHINGRIX)")
 #' @examples vaers_2018 %>% create_dummy("headache", c("Headache")) %>% create_dummy("breathlessness", c("Breathlessness")) %>%  create_dataset("ZOSTER (SHINGRIX)")
 
 
 
-create_dataset = function(.data = vaers_2020, vaxname){
+create_dataset = function(.data = vaers_2020, vaxname = "ZOSTER (SHINGRIX)"){
   .data = .data %>%
            dplyr::filter(vax_name==vaxname, numdays <= 365, numdays > 0) %>%
            dplyr::select(vaers_id,vax_type,vax_manu,vax_name,numdays, 52:dim(.data)[2], everything()) %>%
